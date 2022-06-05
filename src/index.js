3d866020f2b5a0e49c4b5e928372e01536b0b3a2
nuhgnc <Nuh16358@gmail.com>
2022-06-05 13:13:55 +0300

    Genel Görünümün Ayarlanması

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.min.css';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 import client  from './apollo';
